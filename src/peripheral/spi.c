e596b7b74a4ff300fb593c970eec741372ea301d
Ryan Hartlage <ryanplusplus@gmail.com>
2020-04-02 20:19:11 -0400

    Allow CPOL/CPHA to be configured for SPI

--- src/peripheral/spi.c
+++ src/peripheral/spi.c
@@ -68,15 +68,24 @@ static void read(i_tiny_spi_t* _self, tiny_spi_read_callback_t callback, void* c
 
 static const i_tiny_spi_api_t api = { write, read };
 
-i_tiny_spi_t* spi_init(void) {
+i_tiny_spi_t* spi_init(uint8_t cpol, uint8_t cpha, bool msb_first, spi_baud_t baud) {
   // Un-gate clock for SPI
   CLK->PCKENR1 |= (1 << CLK_PERIPHERAL_SPI);
 
-  // Enable peripheral
-  // 1 MHz
-  // Master
-  // MSB first
-  SPI->CR1 = SPI_CR1_SPE | SPI_CR1_MSTR | (3 << 3);
+  // Enable peripheral in master mode
+  SPI->CR1 = SPI_CR1_SPE | SPI_CR1_MSTR | (baud << 3);
+
+  if(!msb_first) {
+    SPI->CR1 |= SPI_CR1_LSBFIRST;
+  }
+
+  if(cpol) {
+    SPI->CR1 |= SPI_CR1_CPOL;
+  }
+
+  if(cpha) {
+    SPI->CR1 |= SPI_CR1_CPHA;
+  }
 
   self.interface.api = &api;
 
--- src/peripheral/spi.h
+++ src/peripheral/spi.h
@@ -7,13 +7,31 @@
 #define spi_h
 
 #include <stdbool.h>
+#include <stdint.h>
 #include "stm8s.h"
 #include "stm8s_itc.h"
 #include "i_tiny_spi.h"
 
 #ifdef SPI
 
-i_tiny_spi_t* spi_init(void);
+// Baud rate settings assume 16 MHz clock
+enum {
+  spi_baud_8mhz,
+  spi_baud_4mhz,
+  spi_baud_2mhz,
+  spi_baud_1mhz,
+  spi_baud_500khz,
+  spi_baud_250khz,
+  spi_baud_125khz,
+  spi_baud_62500hz
+};
+typedef uint8_t spi_baud_t;
+
+i_tiny_spi_t* spi_init(
+  uint8_t cpol,
+  uint8_t cpha,
+  bool msb_first,
+  spi_baud_t baud);
 
 void spi_isr(void) __interrupt(ITC_IRQ_SPI);
 
